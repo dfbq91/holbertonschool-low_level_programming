31a307ff97bea76fd21d018c379a0fda53582f8c
Diego Betancourt <dfbq91@gmail.com>
2019-09-24 20:26:38 +0000

    9 times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,33 +3,25 @@
 /**
 * jack_bauer - works with defined prototype variables
 * Print all minuts in a day
-* @j : is a int type variable
-* Return: j as a last digit of a number
+*
+* Return: minutes
 */
 
 void jack_bauer(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a < 3; a++)
+	for (a = 0; a < 24; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b < 60; b++)
 		{
-			for (c = 0; c < 6; c++)
-			{
-				for (d = 0; d < 10; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(58);
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(58);
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
