ef8176affe9dcf22d104a222b7dfc6a440e72b77
Diego Betancourt <dfbq91@gmail.com>
2019-09-26 14:47:35 +0000

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* isupper - works with defined prototype variables * Description: checks for uppercase character
+*@c: char type variable
+* Return: 1 if c is uppercase, 0 if not.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <=90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
