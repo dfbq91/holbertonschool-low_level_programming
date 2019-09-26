31ab088995ef642a15c41187157d1f25588432e2
Diego Betancourt <dfbq91@gmail.com>
2019-09-26 21:08:30 +0000

    task completed

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+* print_diagonal - works with defined prototype variables
+* draws a diagonal line on the terminal
+*@n: int type variable
+* Return: 0
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (i > j)
+			{
+				_putchar(' ');
+			}
+			else if (i == j)
+			{
+				_putchar(92);
+				_putchar('\n');
+			}
+		}
+	}
+}
