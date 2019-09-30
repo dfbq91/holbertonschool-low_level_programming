8c4daa4730d4c573453ed9ef813ef15cb9054ac3
Diego Betancourt <dfbq91@gmail.com>
2019-09-30 16:58:38 +0000

    will swaps two integers values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *@a: int pointer type variable
+ *@b: int pointer type variable
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
