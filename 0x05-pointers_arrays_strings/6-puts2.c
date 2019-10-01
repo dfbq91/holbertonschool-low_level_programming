6a323e02abf8e284c2c80900f0a3b0f1c3f2d090
Diego Betancourt <dfbq91@gmail.com>
2019-10-01 20:21:34 +0000

    will fix 6 task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,21 +2,33 @@
 
 /**
  * puts2 - get odd numbers
- *@s: char pointer type variable
+ *@str: char pointer type variable
  * Return: Always 0.
  */
 
 void puts2(char *str)
 {
 	int i = 0;
+	int len;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] % 2 == 0)
+		i++;
+	}
+	len = i;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (len % 2 == 1)
+		{
+			len = len - 1;
+		}
+		if (len % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
-		str++;
+		i += 2;
 	}
 	_putchar('\n');
 }
