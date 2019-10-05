a2a0fbf3b41308047e6d1c5b56ca7ceddebaed98
Diego <dfbq91@gmail.com>
2019-10-05 15:21:41 +0000

    will fix again capital strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
 * cap_string - works with defined prototype variables
 * Description: changes all lowercase letters of a string to uppercase.
 *@s: char pointer type variable
-* Return: char
+* Return: string
  */
 
 char *cap_string(char *s)
@@ -13,14 +13,12 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t' ||
-		    s[i - 1] == ';' || s[i - 1] == ',' || s[i - 1] == '!' ||
-		    s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' ||
-		    s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' ||
-		    s[i - 1] == '.') && (s[i] >= 'a' && s[i] <= 'z'))
-		{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+		     s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+		     s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+		     s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+		     s[i - 1] == '}') && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] = s[i] - 32;
-		}
 		i++;
 	}
 	return (s);
