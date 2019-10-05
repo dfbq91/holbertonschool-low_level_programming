87f54163530710366149a85ec4f27bc1133b1f0f
Diego <dfbq91@gmail.com>
2019-10-05 05:00:02 +0000

    capital letters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,26 @@
 #include "holberton.h"
 
 /**
-* string_toupper - works with defined prototype variables
+* cap_string - works with defined prototype variables
 * Description: changes all lowercase letters of a string to uppercase.
 *@s: char pointer type variable
 * Return: char
  */
 
-char *cap_string(char s*)
+char *cap_string(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] == 32 || s[i] == '\n' || s[i] == '\t' || s[i] == ';' ||
+s[i] == ',' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i]
+== ')' || s[i] == '{' || s[i] == '}' || s[i] == '.')
 		{
-			s[i] = s[i] - 32;
+			i += 1;
+			while (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
 		}
-		i++;
 	}
 	return (s);
 }
