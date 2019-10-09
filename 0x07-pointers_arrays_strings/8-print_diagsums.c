072c1ff338a2abeb8c2b066a0c8bcdb53548b4b4
Diego <dfbq91@gmail.com>
2019-10-09 01:26:29 +0000

    will fix diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,33 +2,27 @@
 #include <stdio.h>
 
 /**
-* print_diagsums - works with defined prototype variables
-* Description: sum of the two diagonals of a square matrix of integers
-*@a: char pointer type variable
-*@size: int type variable
-* Return: 0
+ * print_diagsums - works with defined prototype variables
+ * Description: sum of the two diagonals of a square matrix of integers
+ *@a: char pointer type variable
+ *@size: int type variable
+ * Return: 0
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	int j = 0;
-	int sum_a;
-	int sum_b;
+        int i = 0;
+        int j = 0;
+        int sum_a = 0;
+        int sum_b = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		if (a[i] == a[j])
-			sum_a = sum_a + a[i] + a[j];
-		j++;
-	}
-
-	for (j = size - 1; j > 0; j--)
-	{
-		if (a[i] + a[j] == size - 1)
-			sum_b = sum_b + a[i] + a[j];
-		i++;
-	}
-	printf("%d", sum_a);
-	printf("%d", sum_a);
+        for (i = 0; i < size; i++)
+        {
+                sum_a = sum_a + a[(size + 1) * i];
+        }
+        for (j = 0; j < size; j++)
+        {
+                sum_b = sum_b + a[(size - 1) * (j + 1)];
+        }
+        printf("%d, %d\n", sum_a, sum_b);
 }
