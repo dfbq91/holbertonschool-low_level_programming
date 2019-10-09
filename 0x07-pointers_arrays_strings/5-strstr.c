4ec9a914ad9a4cdfca238525138775145141fc2d
Diego <dfbq91@gmail.com>
2019-10-09 03:16:19 +0000

    will find the first occurrence of a string in another string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* _strstr - works with defined prototype variables
+* Description: locates a character in a string
+*@haystack: char pointer type variable
+*@needle: char type variable
+* Return: Returns a pointer or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; *(needle + i) != '\0'; i++)
+	{
+		for (j = 0; *(haystack + j) != '\0'; j++)
+		{
+			if (*(needle + i) == (*(haystack + j)))
+				return (haystack + j);
+		}
+	}
+}
