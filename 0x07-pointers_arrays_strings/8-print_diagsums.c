0336173bb29567c29ad39608a86d3a176dc7eeda
Diego <dfbq91@gmail.com>
2019-10-09 01:29:16 +0000

    will fix diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,18 +11,18 @@
 
 void print_diagsums(int *a, int size)
 {
-        int i = 0;
-        int j = 0;
-        int sum_a = 0;
-        int sum_b = 0;
+	int i = 0;
+	int j = 0;
+	int sum_a = 0;
+	int sum_b = 0;
 
-        for (i = 0; i < size; i++)
-        {
-                sum_a = sum_a + a[(size + 1) * i];
-        }
-        for (j = 0; j < size; j++)
-        {
-                sum_b = sum_b + a[(size - 1) * (j + 1)];
-        }
-        printf("%d, %d\n", sum_a, sum_b);
+	for (i = 0; i < size; i++)
+	{
+		sum_a = sum_a + a[(size + 1) * i];
+	}
+	for (j = 0; j < size; j++)
+	{
+		sum_b = sum_b + a[(size - 1) * (j + 1)];
+	}
+	printf("%d, %d\n", sum_a, sum_b);
 }
