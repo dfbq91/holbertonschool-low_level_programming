cce8243173a4bd9468e040c7fe10661cb7c3c433
Diego Betancourt <dfbq91@gmail.com>
2019-10-09 18:10:04 +0000

    will get factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* factorial - works with defined prototype variables
+* Description: returns factorial of given number
+*@n: int type variable
+* Return: factorial
+ */
+
+int factorial(int n)
+{
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+		return (-1);
+	return (n * factorial (n - 1));
+}
