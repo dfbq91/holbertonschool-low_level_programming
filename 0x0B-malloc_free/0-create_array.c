c32428f8e915bca5a9b31cb1dab9394baffeda5a
Diego Betancourt <dfbq91@gmail.com>
2019-10-15 18:08:40 +0000

    will create an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *create_array - creates an array of chars, and initializes it with a specific char.
+ * @size: unsignet int, size of array of chars
+ * @c: char type variable. An specific char to initialize array
+ * Return: Null if size = 0 or a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	str = malloc(size * sizeof(c));
+	while (i <= size)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+}
