2b514ec0090b7bbed306da1faf8f24d3299a41ee
Diego Betancourt <dfbq91@gmail.com>
2019-10-17 18:47:04 +0000

    will allocate memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc.
+ * @b: unsigned int value to allocate in heap
+ * Return: pointer to the allocate memory or 98 if it fails.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(b));
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
