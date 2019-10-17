dcd9aae6e114a45998d7af675173d0962f3e3599
Diego Betancourt <dfbq91@gmail.com>
2019-10-17 18:52:34 +0000

    updated readme and fixed task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,9 +10,9 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	void *ptr;
 
-	ptr = malloc(sizeof(b));
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,4 +1,4 @@
-# 0x0B. C - malloc, free
+0x0C. C - More malloc, free
 
 ## Requeriments
 gcc 4.8.4 using the flags -Wall -Werror -Wextra and -pedantic
