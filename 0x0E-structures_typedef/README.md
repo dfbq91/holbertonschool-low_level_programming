aca022b5a510862de1b14fa96ad888773f30bbb9
Diego Betancourt <dfbq91@gmail.com>
2019-10-21 17:09:25 +0000

    readme and struct in a header

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+0x0E. C - Structures, typedef
+
+## Requeriments
+gcc 4.8.4 using the flags -Wall -Werror -Wextra and -pedantic
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - struct about dog elements
+* @name: Name of dog
+* @age: Age of dog
+* @owner: Owner of the dog
+* Description: struct about dog description to be used in another functions
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
