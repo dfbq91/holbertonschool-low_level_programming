b56905369af1fa45d178479f7a7e082ee4c456f4
Diego Betancourt <dfbq91@gmail.com>
2019-10-21 19:51:00 +0000

    will fix when d == NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,15 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Return: Initialized variable.
+ * Return: Nothing.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
