7c9376f75182512bd936306b5980446fc28ce2d9
Diego Betancourt <dfbq91@gmail.com>
2019-10-21 22:59:03 +0000

    will use typedef instead define

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#define dog_t struct dog
 
 /**
 * struct dog - struct about dog elements
@@ -16,6 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
