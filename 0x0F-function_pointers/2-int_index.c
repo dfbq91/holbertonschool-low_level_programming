e3a9946005bfa7ad7696e737ecf18a321e561d94
Diego Betancourt <dfbq91@gmail.com>
2019-10-23 20:14:30 +0000

    will index first match in array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: array to print
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to use
+ * Return: nothing.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (cmp != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
