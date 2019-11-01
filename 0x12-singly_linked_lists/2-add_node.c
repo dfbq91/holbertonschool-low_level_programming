b6f8855fdf8f3fdfe7a14889c72ccca0aac8d400
Diego <dfbq91@gmail.com>
2019-10-31 22:45:25 -0500

    fix betty style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,26 +15,24 @@ list_t *add_node(list_t **head, const char *str)
 	len = getlenght(str);
 	new = malloc(sizeof(list_t)); /*Reservo espacio en memoria dinámica*/
 	if (new == NULL)
-	{
 		return (NULL);
-	}
+	new->len = len;
 	new->str = strdup(str);
-	new->len = len; 
 	new->next = *head;
 	*head = new;
 	return (*head);
-	
 }
 /**
  * getlenght - return number of elements in a linked list
- * @*str: pointer to a pointer to head
+ * @str: pointer to a pointer to head
  * Return: lenght of the string
  */
+
 unsigned int getlenght(const char *str)
 {
-  unsigned int len = 0;
-  
-  while (str[len] != '\0')
-    len++;
-  return(len);
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
 }
