43fbe4a55dff186760e743b3db3e910b90368cc0
Diego <dfbq91@gmail.com>
2019-10-31 22:36:31 -0500

    updated header and 2 task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,21 +9,32 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t holbcohort;
 	list_t *new;
-	size_t n;
+	unsigned int len;
 
-	head = &holbcohort;
-	new = malloc(sizeof(list_t));
+	len = getlenght(str);
+	new = malloc(sizeof(list_t)); /*Reservo espacio en memoria dinámica*/
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-	new->str = stdrup(str);
-	new->next = head;
-	head = new;
-	printf([n]
-		free(new->str);
-		free(new);
-	size_t n;
+	new->str = strdup(str);
+	new->len = len; 
+	new->next = *head;
+	*head = new;
+	return (*head);
+	
+}
+/**
+ * getlenght - return number of elements in a linked list
+ * @*str: pointer to a pointer to head
+ * Return: lenght of the string
+ */
+unsigned int getlenght(const char *str)
+{
+  unsigned int len = 0;
+  
+  while (str[len] != '\0')
+    len++;
+  return(len);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -27,4 +28,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+unsigned int getlenght(const char *str);
 #endif
