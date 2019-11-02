56d88fc50bf48470117f5407177dfa6cbab39762
Diego <dfbq91@gmail.com>
2019-11-02 09:54:05 -0500

    fix betty style and change function declaration

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * Return: nothing
  */
 
-void printbeforemain(void) __attribute__ ((constructor))
+void printbeforemain(void)
 {
-	printf(You're beat! and yet, you must allow,\nI bore my house upon my back!\n)
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
