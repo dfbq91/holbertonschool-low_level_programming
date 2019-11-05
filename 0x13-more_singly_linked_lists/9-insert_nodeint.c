d29bd40aa0cf3124eb82fb648d0947b444bf8547
Diego <dfbq91@gmail.com>
2019-11-05 21:02:39 +0000

    will fix a case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	current = *head;
 	temp = *head;
 
+	if (head == NULL)
+		return (NULL);
+
 	if (new == NULL)
 		return (NULL);
 
