130e3a38c52423f269b4180607cc70cf0fa42bb9
Diego <dfbq91@gmail.com>
2019-11-09 12:05:38 -0500

    change in logic of function

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,31 +10,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int operator = 1;
-	unsigned int count = 0;
 
-	while (operator <= n)
-	{
-		operator <<= 1; /*Multiplico operator por dos*/
-		count++;
-	}
-	count--;
-	operator >>= 1; /* Divido operator entre dos*/
-
-	while (operator != 0)
-	{
-		if (operator <= n)
-		{
-			n -= operator;
-			if (index == count)
-				return (1);
-		}
-		else
-		{
-			if (index == count)
-				return (0);
-		}
-		count--;
-		operator >>= 1;
-	}
-	return (-1);
+	if (index > sizeof(n) * 8)
+		return (-1);
+	operator <<= index;
+	if (operator & n)
+		return (1);
+	else
+		return (0);
 }
