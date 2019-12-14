77754f0b422132c2ec20c4013ef5d0e87f01fead
dfbq91 <dfbq91@gmail.com>
2019-12-14 18:06:34 -0500

    will print all elements of a dlinked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return number of elementos of a dlinked list.
+ * @h: pointer to head node
+ * Return: number of elements in a linked dlistint_t list.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	unsigned int nnodes = 0; /*number of nodes*/
+
+	current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nnodes++;
+	}
+
+	return (nnodes);
+}
