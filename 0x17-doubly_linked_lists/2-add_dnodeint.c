037efe71f1803f8aaadbdb442d346059fe21ecdc
dfbq91 <dfbq91@gmail.com>
2019-12-14 18:46:59 -0500

    fix for prev of next node added

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,12 +9,12 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node, *current;
 
 	if (head == NULL)
 		return (NULL);
 
-	new_node = *head;
+	current = *head;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -30,7 +30,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (new_node);
 	}
 
-	new_node->next = *head;
+	new_node->next = current;
+	current->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
