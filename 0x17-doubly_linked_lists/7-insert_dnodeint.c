c28c67f2540ecad7cd9be9bbc438705693d4d04c
dfbq91 <dfbq91@gmail.com>
2019-12-15 12:58:49 -0500

    fix betty #lines

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,52 +10,44 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new_node;
+	dlistint_t *current, *new_node;
 	unsigned int a = 0; /*to compare with idx*/
 
 	if (h == NULL)
 		return (NULL);
-
 	current = *h;
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
 	if (*h == NULL)
-	{
-		*h = new_node;
+	{	*h = new_node;
 		return (new_node);
 	}
-	else
+	for (; current != NULL; current = current->next, a++)
 	{
-		while (current->next != NULL)
+		if (a == idx)
 		{
-			if (a == idx)
+			if (a == 0) /*for node to add at the beggining*/
 			{
-				if (a == 0) /*for node to add at the beggining*/
-				{
-					new_node->next = current;
-					current->prev = new_node;
-					*h = new_node;
-					return (new_node);
-				}
-
-				new_node->next = current->next;
-				current->next->prev = new_node;
-				current->next = new_node;
-				new_node->prev = current;
-
+				new_node->next = current;
+				current->prev = new_node;
+				*h = new_node;
 				return (new_node);
 			}
-			current = current->next;
-			a++;
+			current = current->prev;
+			new_node->next = current->next;
+			current->next->prev = new_node;
+			current->next = new_node;
+			new_node->prev = current;
+			return (new_node);
 		}
-	}
-	return (NULL);
+		if (current->next == NULL && a + 1 == idx) /*for add node at the end*/
+		{	new_node->prev = current;
+			current->next = new_node;
+			return (new_node);
+		}
+	} return (NULL);
 }
