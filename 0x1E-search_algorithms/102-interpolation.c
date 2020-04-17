4ea3d73baec1e50bfff2fde830c8305fa5c7186f
dfbq91 <dfbq91@gmail.com>
2020-04-17 09:16:04 -0500

    added case for size = 0

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,13 +12,14 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	unsigned int low = 0;
 	unsigned int high = size - 1;
-	size_t pos = low + (((double)(high - low) /
-			     (array[high] - array[low])) *
-			    (value - array[low]));
+	size_t pos;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
+	pos = low + (((double)(high - low) /
+			     (array[high] - array[low])) *
+			    (value - array[low]));
 	if (pos > size - 1)
 	{
 		printf("Value checked array[%lu] is out of range\n", pos);
